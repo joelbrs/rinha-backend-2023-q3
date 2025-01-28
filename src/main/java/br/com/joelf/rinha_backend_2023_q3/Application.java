2f43090fb280627e2cc8b786bcebdf0499c37d76
Joel da Silva Pereira Filho <joelfilho02812@gmail.com>
2025-01-28 14:57:43 -0300

    chore: init project

---
+++ src/main/java/br/com/joelf/rinha_backend_2023_q3/Application.java
@@ -0,0 +1,12 @@
+package br.com.joelf.rinha_backend_2023_q3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ src/test/java/br/com/joelf/rinha_backend_2023_q3/RinhaBackend2023Q3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joelf.rinha_backend_2023_q3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinhaBackend2023Q3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
